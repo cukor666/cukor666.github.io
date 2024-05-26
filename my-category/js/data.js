350fd9ebd7d385798472e3f983839a835f2e8b78
Cukor <2450808861@qq.com>
2024-05-26 13:59:24 +0800

    优化分类代码

---
+++ my-category/js/data.js
@@ -0,0 +1,32 @@
+const categoriesInfo = [
+  {
+    id: "front-end",
+    name: "前端",
+    desc: "前端技术分享，包括HTML、CSS、JavaScript、TypeScript、Vue等。",
+  },
+  {
+    id: "back-end",
+    name: "后端",
+    desc: "后端技术分享，包括Java、Python、Go、Node.js等。",
+  },
+  {
+    id: "database",
+    name: "数据库",
+    desc: "数据库技术分享，包括MySQL、MongoDB、Redis等。",
+  },
+  {
+    id: "datastruct",
+    name: "数据结构与算法",
+    desc: "数据结构与算法相关技术分享，包括算法、数据结构、设计模式等。",
+  },
+  {
+    id: "ops",
+    name: "运维",
+    desc: "运维技术分享，包括Linux、Docker、Kubernetes等。",
+  },
+  {
+    id: "other",
+    name: "其他",
+    desc: "其他技术分享，包括计算机基础、操作系统、网络、安全等。",
+  },
+];
